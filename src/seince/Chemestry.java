0b8096c508752ecb7b87a3e6819d7a3a05d986fd
arham2008 <akbarmunner@gmail.com>
2020-09-06 19:59:56 -0400

    i akbar muneer add  unversty project in .git

---
+++ src/seince/Chemestry.java
@@ -0,0 +1,23 @@
+package seince;
+
+public class Chemestry {
+
+	//casting ?
+	// converting data type
+	
+	 double cost=3560.500;
+	 int newcost=(int) cost;
+
+	public static void main(String[] args) {
+		
+		
+			
+		     Chemestry cl = new Chemestry();
+		
+			 System.out.println(cl.cost);
+			 
+		
+	}
+	
+	
+}
